ae922a0258b1ddc5f4a8a4217790a84e8f2fbc06
eliseo luquez <eluquez@trabajo.gob.ar>
2022-06-23 19:24:27 -0300

    fix historia clinica pdf

--- src/app/componentes/historia-clinica/historia-clinica.component.ts
+++ src/app/componentes/historia-clinica/historia-clinica.component.ts
@@ -7,6 +7,7 @@ import { HistoriaClinicaService } from 'src/app/services/historia-clinica.servic
 import { jsPDF } from "jspdf";
 import html2canvas from 'html2canvas';
 import { TurnoService } from 'src/app/services/turno.service';
+import { Turno } from 'src/app/clases/turno';
 
 @Component({
   selector: 'app-historia-clinica',
@@ -24,32 +25,47 @@ export class HistoriaClinicaComponent implements OnInit {
 
   constructor(public historiaClinicaService: HistoriaClinicaService, public authSvc: AuthService, private turnoSvc: TurnoService) {
     this.usuario = this.authSvc.usuarioLogueado;
-    this.listadoHistoriaClinica = this.turnoSvc.db.collection("turnos", ref => ref.where('turno.especialistaId', '==', this.usuario.id));
-    this.buscarHistoria();
-    console.log(this.listaHistoriaClinica);
+    console.log(this.usuario);
   }
 
   ngOnInit(): void {
+    this.listadoHistoriaClinica = this.turnoSvc.db.collection("turnos", ref => ref.where('idPaciente', '==', this.usuario.id));
+    this.buscarHistoria();
+    console.log(this.listaHistoriaClinica);
   }
 
   buscarHistoria() {
     this.listaHistoriaClinica = new Array<HistoriaClinica>();
     this.listadoHistoriaClinica.snapshotChanges().pipe(
       map((data: any) => {
-        data.map((turno: any) => {
-          var history: HistoriaClinica = new HistoriaClinica();
+        data.map((item: any) => {
+          var turno: Turno = new Turno();
+          //turno.id = item.payload.doc.id;
+          turno.idEspecialista = item.payload.doc.data().idEspecialista;
+          turno.idPaciente = item.payload.doc.data().idPaciente;
+          turno.estado = item.payload.doc.data().estado;
+          //turno.paciente = new Usuario();
+          turno.paciente = item.payload.doc.data().paciente;
+          turno.especialista = item.payload.doc.data().especialista;
+          turno.especialidad = item.payload.doc.data().especialidad;
+          turno.fecha = item.payload.doc.data().fecha;
+          turno.hora = item.payload.doc.data().hora;
+          turno.comentariosPaciente = item.payload.doc.data().comentariosPaciente;
+          turno.comentariosEspecialista = item.payload.doc.data().comentariosEspecialista;
+          turno.comentariosAdmin = item.payload.doc.data().comentariosAdmin;
+          turno.historiaClinica = item.payload.doc.data().historiaClinica;
           //console.log(historia.payload.doc.data().Turno.PacienteEmail);
-          history.temperatura = turno.payload.doc.data().historiaClinica.temperatura;
-          history.presion = turno.payload.doc.data().historiaClinica.presion;
-          history.altura = turno.payload.doc.data().historiaClinica.altura;
-          history.peso = turno.payload.doc.data().historiaClinica.peso;
-          history.clave1 = turno.payload.doc.data().historiaClinica.clave1;
-          history.clave2 = turno.payload.doc.data().historiaClinica.clave2;
-          //history.id = historia.payload.doc.id;
-          history.valor1 = turno.payload.doc.data().historiaClinica.valor1;
-          history.valor2 = turno.payload.doc.data().historiaClinica.valor2;
-          history.turnoId = turno.payload.doc.data().historiaClinica.turnoId;
-          this.listaHistoriaClinica.push(history);
+          turno.historiaClinica.temperatura = item.payload.doc.data().historiaClinica.temperatura;
+          turno.historiaClinica.presion = item.payload.doc.data().historiaClinica.presion;
+          turno.historiaClinica.altura = item.payload.doc.data().historiaClinica.altura;
+          turno.historiaClinica.peso = item.payload.doc.data().historiaClinica.peso;
+          turno.historiaClinica.clave1 = item.payload.doc.data().historiaClinica.clave1;
+          turno.historiaClinica.clave2 = item.payload.doc.data().historiaClinica.clave2;
+          //turno.id = historia.payload.doc.id;
+          turno.historiaClinica.valor1 = item.payload.doc.data().historiaClinica.valor1;
+          turno.historiaClinica.valor2 = item.payload.doc.data().historiaClinica.valor2;
+          //turno.turnoId = turno.payload.doc.data().historiaClinica.turnoId;
+          this.listaHistoriaClinica.push(turno);
         })
       })
     ).subscribe((datos: any) => {
